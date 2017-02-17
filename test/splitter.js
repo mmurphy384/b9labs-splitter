cb0dbf822fd1d457a8aed542a098394222d061ee
mmurphy384 <mmurphy384@yahoo.com>
2017-02-17 00:46:32 +0000

    more updates

--- test/splitter.js
+++ test/splitter.js
@@ -1,12 +1,59 @@
-contract('Splitter', function() {
-  it("should make sure we can access Bob's address.", function() {
-    var split = Splitter.deployed();
+contract('Splitter', function () {
 
-    return split.bob.call()
-      .then(function (_result) {
-        assert.equal(1, 1, "Bob's address is " + _result);
-      })
-      .done();
-  });
+	web3.eth.getTransactionReceiptMined = function (txnHash, interval) {
+			var transactionReceiptAsync;
+			interval = interval ? interval : 500;
+			transactionReceiptAsync = function(txnHash, resolve, reject) {
+					try {
+							var receipt = web3.eth.getTransactionReceipt(txnHash);
+							if (receipt == null) {
+									setTimeout(function () {
+											transactionReceiptAsync(txnHash, resolve, reject);
+									}, interval);
+							} else {
+									resolve(receipt);
+							}
+					} catch(e) {
+							reject(e);
+					}
+			};
+
+			if (Array.isArray(txnHash)) {
+					var promises = [];
+					txnHash.forEach(function (oneTxHash) {
+							promises.push(web3.eth.getTransactionReceiptMined(oneTxHash, interval));
+					});
+					return Promise.all(promises);
+			} else {
+					return new Promise(function (resolve, reject) {
+									transactionReceiptAsync(txnHash, resolve, reject);
+							});
+			}
+	};
+		
+	var numStartingAccounts = 0;
+	var numEndingAccounts = 0;
+	var maxAccounts = 0;
+	var txHash;
+
+	it("should add two accounts and prevent a third from being added.", function () {
+		var split = Splitter.deployed();
+		split.maxAccounts.call().then(function(_result){maxAccounts = _result;});
+		split.addMyAccount({ from: web3.eth.accounts[1] });
+		split.addMyAccount({ from: web3.eth.accounts[2] }).then(function (_txHash) {
+			txHash = _txHash;
+		});
+
+		// wait for the last transaction to get mined
+		var result = web3.eth.getTransactionReceiptMined(txHash)
+			.then(function () { 
+				split.getNumAccounts.call().then(function (_result) {
+					numEndingAccounts = _result;
+				});							
+			});
+
+		//assert.equal(maxAccounts,2, "The maximum number of accounts is " + maxAccounts);
+		assert.equal(numEndingAccounts, 2, "The number of accounts is " + numEndingAccounts);
+
+	});
 });
-
