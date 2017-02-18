0e6f15c3c4a95b51f24bc75d75325c95f37bff3c
mmurphy384 <mmurphy384@yahoo.com>
2017-02-18 20:50:18 +0000

    Updated readme

--- test/splitter.js
+++ test/splitter.js
@@ -2,34 +2,34 @@ contract('Splitter', function () {
 	// This function is used to help deal with the delay associated with mining.
 	// I'm not sure if I have to add this within the function or not.
 	web3.eth.getTransactionReceiptMined = function (txnHash, interval) {
-			var transactionReceiptAsync;
-			interval = interval ? interval : 500;
-			transactionReceiptAsync = function(txnHash, resolve, reject) {
-					try {
-							var receipt = web3.eth.getTransactionReceipt(txnHash);
-							if (receipt == null) {
-									setTimeout(function () {
-											transactionReceiptAsync(txnHash, resolve, reject);
-									}, interval);
-							} else {
-									resolve(receipt);
-							}
-					} catch(e) {
-							reject(e);
-					}
-			};
-
-			if (Array.isArray(txnHash)) {
-					var promises = [];
-					txnHash.forEach(function (oneTxHash) {
-							promises.push(web3.eth.getTransactionReceiptMined(oneTxHash, interval));
-					});
-					return Promise.all(promises);
-			} else {
-					return new Promise(function (resolve, reject) {
-									transactionReceiptAsync(txnHash, resolve, reject);
-							});
+		var transactionReceiptAsync;
+		interval = interval ? interval : 500;
+		transactionReceiptAsync = function(txnHash, resolve, reject) {
+			try {
+				var receipt = web3.eth.getTransactionReceipt(txnHash);
+				if (receipt == null) {
+						setTimeout(function () {
+								transactionReceiptAsync(txnHash, resolve, reject);
+						}, interval);
+				} else {
+						resolve(receipt);
+				}
+			} catch(e) {
+				reject(e);
 			}
+		};
+
+		if (Array.isArray(txnHash)) {
+			var promises = [];
+			txnHash.forEach(function (oneTxHash) {
+				promises.push(web3.eth.getTransactionReceiptMined(oneTxHash, interval));
+			});
+			return Promise.all(promises);
+			} else {
+			return new Promise(function (resolve, reject) {
+				transactionReceiptAsync(txnHash, resolve, reject);
+			});
+		}
 	};
 
 	describe("Splitter Tests", function () {
@@ -77,8 +77,6 @@ contract('Splitter', function () {
 
 			// Initialize the contract and add 2 accounts to the contract
 			var split = Splitter.at(instance.address);
-			assert.equal(split.address, instance.address, "Addresses are correct");
-			assert.equal(numAccounts, 2, "The actual number of accounts is " + numAccounts);
 			return split.getMyBalance.call(name1).then(function (_balance10) {
 				// set name1s initial balance
 				name1StartBalance = _balance10.toNumber();
